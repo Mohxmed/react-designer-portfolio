d64af2d07388fac2e17a77cb65a6b268830656cb
Mhmd Amr <MohxmedAmr@gmail.com>
2024-09-07 15:44:03 +0300

    Update CSS Checkpoints

--- src/components/HeroSection/HeroSectionComponent.jsx
+++ src/components/HeroSection/HeroSectionComponent.jsx
@@ -63,7 +63,8 @@ const HeroInfoText = () => {
     <div className="info">
       <span className="greeting">Hey!</span>
       <h1 className="hero-title">
-        I'm <span className="main-color">Ariene Ray</span>. an UI/UX Designer.
+        I'm <span className="main-color">Ariene Ray</span>. <br />
+        an UI/UX Designer.
       </h1>
       <img className="fx2" src={LineImage} />
       <p>
