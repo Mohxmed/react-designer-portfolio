554cbb4a47c95d3ab552c648a71b45b834d3530c
Mhmd Amr <MohxmedAmr@gmail.com>
2024-09-08 00:46:32 +0300

    update

---
+++ src/components/LandingContact/LandingContact.jsx
@@ -0,0 +1,16 @@
+import Container from "../../layouts/Container";
+import "./landing-contact.css";
+
+const LandingContact = () => {
+  return (
+    <div className="landing-contact">
+      <Container>
+        <div className="content">
+          <h1>interested working with me?</h1>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default LandingContact;
